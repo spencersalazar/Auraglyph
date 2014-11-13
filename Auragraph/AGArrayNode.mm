ded3062f5a82984f24bde2aa2b8b209ee696935d
Spencer Salazar <spencer.salazar@gmail.com>
2014-11-13 03:20:59 -0800

    refactor

--- Auragraph/AGArrayNode.mm
+++ Auragraph/AGArrayNode.mm
@@ -15,54 +15,6 @@
 
 #include <sstream>
 
-class AGSqueezeAnimation
-{
-public:
-    AGSqueezeAnimation()
-    {
-        open();
-    }
-    
-    void open()
-    {
-        m_xScale = lincurvef(AGStyle::open_animTimeX, AGStyle::open_squeezeHeight, 1);
-        m_yScale = lincurvef(AGStyle::open_animTimeY, AGStyle::open_squeezeHeight, 1);
-    }
-    
-    void close()
-    {
-        m_xScale = lincurvef(AGStyle::open_animTimeX/2, 1, AGStyle::open_squeezeHeight);
-        m_yScale = lincurvef(AGStyle::open_animTimeY/2, 1, AGStyle::open_squeezeHeight);
-    }
-    
-    bool finishedClosing()
-    {
-        return m_xScale <= AGStyle::open_squeezeHeight;
-    }
-    
-    bool isHorzOpen()
-    {
-        return m_xScale >= 0.99;
-    }
-    
-    GLKMatrix4 matrix()
-    {
-        return GLKMatrix4MakeScale(m_yScale <= AGStyle::open_squeezeHeight ? (float)m_xScale : 1.0f,
-                                   m_xScale >= 0.99f ? (float)m_yScale : AGStyle::open_squeezeHeight,
-                                   1.0f);
-    }
-    
-    void update(float t, float dt)
-    {
-        if(m_yScale <= AGStyle::open_squeezeHeight) m_xScale.update(dt);
-        if(m_xScale >= 0.99f) m_yScale.update(dt);
-    }
-    
-private:
-    lincurvef m_xScale;
-    lincurvef m_yScale;
-};
-
 
 //------------------------------------------------------------------------------
 // ### AGUINumberInput ###
--- Auragraph/AGStyle.h
+++ Auragraph/AGStyle.h
@@ -11,6 +11,8 @@
 
 #include "TexFont.h"
 #include <string>
+#include "Animation.h"
+
 
 using namespace std;
 
@@ -29,5 +31,55 @@ public:
     static const float open_animTimeX;
     static const float open_animTimeY;
 };
+
+
+class AGSqueezeAnimation
+{
+public:
+    AGSqueezeAnimation()
+    {
+        open();
+    }
+    
+    void open()
+    {
+        m_xScale = lincurvef(AGStyle::open_animTimeX, AGStyle::open_squeezeHeight, 1);
+        m_yScale = lincurvef(AGStyle::open_animTimeY, AGStyle::open_squeezeHeight, 1);
+    }
+    
+    void close()
+    {
+        m_xScale = lincurvef(AGStyle::open_animTimeX/2, 1, AGStyle::open_squeezeHeight);
+        m_yScale = lincurvef(AGStyle::open_animTimeY/2, 1, AGStyle::open_squeezeHeight);
+    }
+    
+    bool finishedClosing()
+    {
+        return m_xScale <= AGStyle::open_squeezeHeight;
+    }
+    
+    bool isHorzOpen()
+    {
+        return m_xScale >= 0.99;
+    }
+    
+    GLKMatrix4 matrix()
+    {
+        return GLKMatrix4MakeScale(m_yScale <= AGStyle::open_squeezeHeight ? (float)m_xScale : 1.0f,
+                                   m_xScale >= 0.99f ? (float)m_yScale : AGStyle::open_squeezeHeight,
+                                   1.0f);
+    }
+    
+    void update(float t, float dt)
+    {
+        if(m_yScale <= AGStyle::open_squeezeHeight) m_xScale.update(dt);
+        if(m_xScale >= 0.99f) m_yScale.update(dt);
+    }
+    
+private:
+    lincurvef m_xScale;
+    lincurvef m_yScale;
+};
+
 
 #endif /* defined(__Auragraph__AGStyle__) */
