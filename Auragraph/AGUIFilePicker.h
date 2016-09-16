9f61dba491c3b456b028eb44ade657cbca627cb3
Spencer Salazar <spencer.salazar@gmail.com>
2016-09-16 13:56:06 -0700

    AGUIFilePicker initial files

---
+++ Auragraph/AGUIFilePicker.h
@@ -0,0 +1,19 @@
+//
+//  AGUIFilePicker.hpp
+//  Auragraph
+//
+//  Created by Spencer Salazar on 9/16/16.
+//  Copyright © 2016 Spencer Salazar. All rights reserved.
+//
+
+#ifndef AGUIFilePicker_hpp
+#define AGUIFilePicker_hpp
+
+#include "AGRenderObject.h"
+
+class AGUIFilePicker : public AGInteractiveObject
+{
+    
+};
+
+#endif /* AGUIFilePicker_hpp */
---
+++ Auragraph/AGUIFilePicker.mm
@@ -0,0 +1,11 @@
+//
+//  AGUIFilePicker.cpp
+//  Auragraph
+//
+//  Created by Spencer Salazar on 9/16/16.
+//  Copyright © 2016 Spencer Salazar. All rights reserved.
+//
+
+#include "AGUIFilePicker.h"
+
+
