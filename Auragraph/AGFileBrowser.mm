ee6d63f17d46fac13b32762996b9c0d063872a0a
Spencer Salazar <spencer.salazar@gmail.com>
2017-01-12 09:21:03 -0800

    fix for latest graphics framework updates

--- Auragraph/AGFileBrowser.mm
+++ Auragraph/AGFileBrowser.mm
@@ -60,7 +60,7 @@ void AGFileBrowser::update(float t, float dt)
 void AGFileBrowser::render()
 {
     // draw inner box
-    glVertexAttrib4fv(GLKVertexAttribColor, (const float *) &AGStyle::frameBackgroundColor());
+    glVertexAttrib4fv(AGVertexAttribColor, (const float *) &AGStyle::frameBackgroundColor());
     drawTriangleFan((GLvertex3f[]){
         { -m_size.x/2, -m_size.y/2, 0 },
         {  m_size.x/2, -m_size.y/2, 0 },
@@ -69,7 +69,7 @@ void AGFileBrowser::render()
     }, 4);
     
     // draw outer frame
-    glVertexAttrib4fv(GLKVertexAttribColor, (const float *) &AGStyle::foregroundColor);
+    glVertexAttrib4fv(AGVertexAttribColor, (const float *) &AGStyle::foregroundColor);
     glLineWidth(4.0f);
     drawLineLoop((GLvertex3f[]){
         { -m_size.x/2, -m_size.y/2, 0 },
@@ -95,7 +95,7 @@ void AGFileBrowser::render()
             GLKMatrix4 xform = GLKMatrix4MakeTranslation(0, yPos, 0);
             float margin = 0.975f;
             // draw selection box
-            glVertexAttrib4fv(GLKVertexAttribColor, (const GLfloat *) &AGStyle::foregroundColor);
+            glVertexAttrib4fv(AGVertexAttribColor, (const GLfloat *) &AGStyle::foregroundColor);
             
             drawTriangleFan((GLvertex3f[]){
                 { -m_size.x/2*margin,  m_itemHeight/2*margin, 0 },
