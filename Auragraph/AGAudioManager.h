7bc9936c731f715dbb1c7bade794d4ed14e3f34a
Spencer Salazar <spencer.salazar@gmail.com>
2017-06-29 19:21:42 -0700

    add AudioManager C++ proxy + AudioRateProcessor delegates

--- Auragraph/AGAudioManager.h
+++ Auragraph/AGAudioManager.h
@@ -6,14 +6,25 @@
 //  Copyright (c) 2013 Spencer Salazar. All rights reserved.
 //
 
+#ifdef __OBJC__
 #import <Foundation/Foundation.h>
-#import "AGAudioCapturer.h"
-#import "AGAudioOutputDestination.h"
+#endif // __OBJC__
+#include "AGAudioCapturer.h"
+#include "AGAudioOutputDestination.h"
 
 class AGAudioOutputNode;
 class AGAudioNode;
 class AGTimer;
 
+// audio-rate processor that does not actually generate audio
+class AGAudioRateProcessor
+{
+public:
+    virtual ~AGAudioRateProcessor() { }
+    virtual void process(sampletime t) = 0;
+};
+
+#ifdef __OBJC__
 
 @interface AGAudioManager : NSObject
 
@@ -27,8 +38,35 @@ class AGTimer;
 - (void)removeCapturer:(AGAudioCapturer *)capturer;
 - (void)addTimer:(AGTimer *)timer;
 - (void)removeTimer:(AGTimer *)timer;
+- (void)addAudioRateProcessor:(AGAudioRateProcessor *)processor;
+- (void)removeAudioRateProcessor:(AGAudioRateProcessor *)processor;
 
 - (void)startSessionRecording;
 - (void)stopSessionRecording;
 
 @end
+
+#else //
+
+typedef void AGAudioManager;
+
+#endif // __OBJC__
+
+// proxy class for C++-only code
+class AGAudioManager_
+{
+public:
+    static AGAudioManager_ &instance();
+    
+    AGAudioManager_(AGAudioManager *);
+    
+    void startSessionRecording();
+    void stopSessionRecording();
+    
+    void addAudioRateProcessor(AGAudioRateProcessor *processor);
+    void removeAudioRateProcessor(AGAudioRateProcessor *processor);
+    
+private:
+    AGAudioManager *m_audioManager = nullptr;
+};
+
