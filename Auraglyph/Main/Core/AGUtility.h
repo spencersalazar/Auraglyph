a8f22710bf78a37a92fa32438c98999100d934eb
Spencer Salazar <spencer.salazar@gmail.com>
2019-05-19 13:46:53 -0700

    add missing files

---
+++ Auraglyph/Main/Core/AGUtility.h
@@ -0,0 +1,18 @@
+//
+//  AGUtility.hpp
+//  Auraglyph
+//
+//  Created by Spencer Salazar on 5/18/19.
+//  Copyright © 2019 Spencer Salazar. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+
+namespace AGUtility {
+    
+    std::string getVersionString();
+    
+}
+
---
+++ Auraglyph/Main/Core/AGUtility.mm
@@ -0,0 +1,26 @@
+//
+//  AGUtility.cpp
+//  Auraglyph
+//
+//  Created by Spencer Salazar on 5/18/19.
+//  Copyright © 2019 Spencer Salazar. All rights reserved.
+//
+
+#include "AGUtility.h"
+
+#include <Foundation/Foundation.h>
+#include "NSString+STLString.h"
+
+std::string AGUtility::getVersionString()
+{
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSString *versionString = [mainBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    
+    std::string v = [versionString stlString];
+    
+#ifdef AG_BETA
+    v += " (beta)";
+#endif // AG_BETA
+    
+    return v;
+}
