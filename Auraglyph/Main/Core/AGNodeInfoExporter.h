20a8b21abab1d1afd527d841334d03f95ac89b3e
Spencer Salazar <spencer.salazar@gmail.com>
2019-11-28 10:09:01 -0500

    add missing file

---
+++ Auraglyph/Main/Core/AGNodeInfoExporter.h
@@ -0,0 +1,17 @@
+//
+//  AGNodeInfoExporter.hpp
+//  Auraglyph
+//
+//  Created by Spencer Salazar on 11/28/19.
+//  Copyright © 2019 Spencer Salazar. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+
+class AGNodeInfoExporter
+{
+public:
+    static void exportNodeInfoToFile(const std::string &file);
+};
